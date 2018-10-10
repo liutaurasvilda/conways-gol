93032c6fda53111178eb57a3b7543fb354eb8a36
Liutauras Vilda <lvilda@axiomsl.com>
2018-10-10 10:55:23 +0100

    increased test coverage 5

--- src/test/java/io/github/liutaurasvilda/gol/core/CellTest.java
+++ src/test/java/io/github/liutaurasvilda/gol/core/CellTest.java
@@ -7,45 +7,13 @@ import static org.junit.Assert.*;
 public class CellTest {
 
     @Test
-    public void alive_cell_mutates_to_dead_with_less_than_two_alive_neighbors() {
-        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(1);
-        assertEquals(Cell.dead(), Cell.alive().mutate(rules.build()));
+    public void alive_cell_phase_is_ALIVE() {
+        assertSame(Cell.alive().phase(), Mutable.Phase.ALIVE);
     }
 
     @Test
-    public void alive_cell_stays_alive_with_two_alive_neighbors() {
-        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(2);
-        assertEquals(Cell.alive(), Cell.alive().mutate(rules.build()));
-    }
-
-    @Test
-    public void alive_cell_stays_alive_with_three_alive_neighbors() {
-        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(3);
-        assertEquals(Cell.alive(), Cell.alive().mutate(rules.build()));
-    }
-
-    @Test
-    public void alive_cell_mutates_to_dead_with_more_than_three_alive_neighbors() {
-        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(4);
-        assertEquals(Cell.dead(), Cell.alive().mutate(rules.build()));
-    }
-
-    @Test
-    public void dead_cell_mutates_to_alive_with_three_alive_neighbors() {
-        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(3);
-        assertEquals(Cell.alive(), Cell.dead().mutate(rules.build()));
-    }
-
-    @Test
-    public void dead_cell_stays_dead_with_one_alive_neighbor() {
-        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(1);
-        assertEquals(Cell.dead(), Cell.dead().mutate(rules.build()));
-    }
-
-    @Test
-    public void dead_cell_stays_dead_with_four_alive_neighbor() {
-        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(4);
-        assertEquals(Cell.dead(), Cell.dead().mutate(rules.build()));
+    public void dead_cell_phase_is_DEAD() {
+        assertSame(Cell.dead().phase(), Mutable.Phase.DEAD);
     }
 
     @Test
@@ -88,6 +56,48 @@ public class CellTest {
     public void two_same_mutable_dead_references_refer_to_same_object() {
         Mutable cell = Cell.dead();
         assertEquals(cell, cell);
+    }
+
+    @Test
+    public void alive_cell_mutates_to_dead_with_less_than_two_alive_neighbors() {
+        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(1);
+        assertEquals(Cell.dead(), Cell.alive().mutate(rules.build()));
+    }
+
+    @Test
+    public void alive_cell_stays_alive_with_two_alive_neighbors() {
+        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(2);
+        assertEquals(Cell.alive(), Cell.alive().mutate(rules.build()));
+    }
+
+    @Test
+    public void alive_cell_stays_alive_with_three_alive_neighbors() {
+        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(3);
+        assertEquals(Cell.alive(), Cell.alive().mutate(rules.build()));
+    }
+
+    @Test
+    public void alive_cell_mutates_to_dead_with_more_than_three_alive_neighbors() {
+        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(4);
+        assertEquals(Cell.dead(), Cell.alive().mutate(rules.build()));
+    }
+
+    @Test
+    public void dead_cell_mutates_to_alive_with_three_alive_neighbors() {
+        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(3);
+        assertEquals(Cell.alive(), Cell.dead().mutate(rules.build()));
+    }
+
+    @Test
+    public void dead_cell_stays_dead_with_one_alive_neighbor() {
+        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(1);
+        assertEquals(Cell.dead(), Cell.dead().mutate(rules.build()));
+    }
+
+    @Test
+    public void dead_cell_stays_dead_with_four_alive_neighbor() {
+        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(4);
+        assertEquals(Cell.dead(), Cell.dead().mutate(rules.build()));
     }
 
     @Test
