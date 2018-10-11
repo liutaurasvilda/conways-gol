b451cde93712a4c84fa397fa30cb32ad2a263009
Liutauras Vilda <lvilda@axiomsl.com>
2018-10-11 13:36:46 +0100

    Refactor of MutationRules Builder

--- src/main/java/io/github/liutaurasvilda/gol/core/MutationRules.java
+++ src/main/java/io/github/liutaurasvilda/gol/core/MutationRules.java
@@ -4,24 +4,6 @@ import java.util.function.Function;
 
 final class MutationRules {
 
-    private final long livingNeighbors;
-
-    private MutationRules(Builder builder) {
-        this.livingNeighbors = builder.livingNeighbors;
-    }
-
-    private Function<Mutable, Mutable> get() {
-        return mutable -> {
-            if (mutable.equals(Cell.ALIVE)) {
-                if (livingNeighbors == 2) return Cell.ALIVE;
-                if (livingNeighbors == 3) return Cell.ALIVE;
-            } else if (mutable.equals(Cell.DEAD)) {
-                if (livingNeighbors == 3) return Cell.ALIVE;
-            }
-            return Cell.DEAD;
-        };
-    }
-
     static final class Builder {
 
         private long livingNeighbors;
@@ -34,8 +16,20 @@ final class MutationRules {
             return this;
         }
 
+        private Function<Mutable, Mutable> f() {
+            return mutable -> {
+                if (mutable.equals(Cell.ALIVE)) {
+                    if (livingNeighbors == 2) return Cell.ALIVE;
+                    if (livingNeighbors == 3) return Cell.ALIVE;
+                } else if (mutable.equals(Cell.DEAD)) {
+                    if (livingNeighbors == 3) return Cell.ALIVE;
+                }
+                return Cell.DEAD;
+            };
+        }
+
         Function<Mutable, Mutable> build() {
-            return new MutationRules(this).get();
+            return f();
         }
     }
 }
