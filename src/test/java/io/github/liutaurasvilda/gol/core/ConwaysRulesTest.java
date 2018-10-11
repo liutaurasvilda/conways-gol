50d491a6a324049f2c649039d6d4005b8682581d
Liutauras Vilda <lvilda@axiomsl.com>
2018-10-11 10:00:38 +0100

    Rules added null check test

--- src/test/java/io/github/liutaurasvilda/gol/core/ConwaysRulesTest.java
+++ src/test/java/io/github/liutaurasvilda/gol/core/ConwaysRulesTest.java
@@ -43,8 +43,14 @@ public class ConwaysRulesTest {
     }
 
     @Test
-    public void rules_with_four_alive_neighbor_applied_to_dead_cell_returns_dead_cell() {
+    public void rules_with_four_alive_neighbors_applied_to_dead_cell_returns_dead_cell() {
         ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(4);
         assertEquals(Cell.DEAD, rules.build().apply(Cell.DEAD));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void rules_applied_to_null_throws_exception() {
+        ConwaysRules.Builder rules = new ConwaysRules.Builder().withLivingNeighbors(4);
+        rules.build().apply(null);
     }
 }
