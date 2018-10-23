a78b85eec4b935024667150bb628b7493a3fe78e
Liutauras Vilda <lvilda@axiomsl.com>
2018-10-23 10:56:17 +0100

    RegenerationRules added test

--- core-lib/src/test/java/io/github/liutaurasvilda/gol/RegenerationRulesTest.java
+++ core-lib/src/test/java/io/github/liutaurasvilda/gol/RegenerationRulesTest.java
@@ -45,6 +45,11 @@ public class RegenerationRulesTest {
         assertEquals(Cell.EMPTY, rules.withLivingNeighbors(4).build().apply(Cell.EMPTY));
     }
 
+    @Test
+    public void rules_applied_to_other_than_alive_or_empty_cell_returns_empty_cell() {
+        assertEquals(Cell.EMPTY, rules.withLivingNeighbors(0).build().apply(new Regenerable() {}));
+    }
+
     @Test(expected = NullPointerException.class)
     public void rules_applied_to_null_throws_exception() {
         rules.withLivingNeighbors(4).build().apply(null);
