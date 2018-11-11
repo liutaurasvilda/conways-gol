99fac082a7c9c92e8da2c79f2033f1a21a2c2aa8
Liutauras Vilda <liutauras.vilda@gmail.com>
2018-11-11 13:51:37 +0000

    Added CellTest

---
+++ core-lib/src/test/java/io/github/liutaurasvilda/gol/CellTest.java
@@ -0,0 +1,23 @@
+package io.github.liutaurasvilda.gol;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CellTest {
+
+    @Test
+    public void alive_cell_with_two_alive_neighbors_regenerates_to_alive_cell() {
+        assertEquals(Cell.ALIVE, Cell.regenerationRules().apply(Cell.ALIVE, 2L));
+    }
+
+    @Test
+    public void alive_cell_with_three_alive_neighbors_regenerates_to_alive_cell() {
+        assertEquals(Cell.ALIVE, Cell.regenerationRules().apply(Cell.ALIVE, 3L));
+    }
+
+    @Test
+    public void unpopulated_cell_with_three_alive_neighbors_regenerates_to_alive_cell() {
+        assertEquals(Cell.ALIVE, Cell.regenerationRules().apply(null, 3L));
+    }
+}
