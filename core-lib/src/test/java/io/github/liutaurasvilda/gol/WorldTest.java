f5585cc723c4d8a86558a6b5847c5ad2cbdf49f3
Liutauras Vilda <lvilda@axiomsl.com>
2018-11-20 10:49:07 +0000

    Test coverage increase

--- core-lib/src/test/java/io/github/liutaurasvilda/gol/WorldTest.java
+++ core-lib/src/test/java/io/github/liutaurasvilda/gol/WorldTest.java
@@ -70,27 +70,27 @@ public class WorldTest {
     }
 
     @Test
-    public void gg1() {
+    public void world_is_oval_specifying_location_with_positive_indices() {
         World world1 = World.empty();
         world1.setSize(20);
         world1.aliveAt(Location.of(0, 0));
 
         World world2 = World.empty();
         world2.setSize(20);
-        world2.aliveAt(Location.of(20, 0));
+        world2.aliveAt(Location.of(20, 20));
 
         assertThat(world1.toString(), is(equalTo(world2.toString())));
     }
 
     @Test
-    public void gg2() {
+    public void world_is_oval_specifying_location_with_negative_indices() {
         World world1 = World.empty();
         world1.setSize(20);
         world1.aliveAt(Location.of(0, 0));
 
         World world2 = World.empty();
         world2.setSize(20);
-        world2.aliveAt(Location.of(-20, 0));
+        world2.aliveAt(Location.of(-20, -20));
 
         assertThat(world1.toString(), is(equalTo(world2.toString())));
     }
