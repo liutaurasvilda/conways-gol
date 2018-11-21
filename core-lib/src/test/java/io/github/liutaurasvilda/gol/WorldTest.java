d0854a2ee239dec9755bbc826787acdd4b1ecb74
Liutauras Vilda <liutauras.vilda@gmail.com>
2018-11-21 23:14:57 +0000

    Variable rename

--- core-lib/src/test/java/io/github/liutaurasvilda/gol/WorldTest.java
+++ core-lib/src/test/java/io/github/liutaurasvilda/gol/WorldTest.java
@@ -11,33 +11,33 @@ import static org.hamcrest.Matchers.*;
 
 public class WorldTest {
 
-    private static final int DEFAULT_SIZE = 10;
+    private static final int WORLD_SIZE = 10;
 
     @Test
     public void world_generation_with_no_seed_has_no_population() {
         List<Location> seed = Collections.emptyList();
-        World world = World.generation(seed, DEFAULT_SIZE);
+        World world = World.generation(seed, WORLD_SIZE);
         assertThat(world.hasPopulation(), is(false));
     }
 
     @Test
     public void world_generation_with_one_seed_has_population() {
         List<Location> seed = Collections.singletonList(Location.of(0, 0));
-        World world = World.generation(seed, DEFAULT_SIZE);
+        World world = World.generation(seed, WORLD_SIZE);
         assertThat(world.hasPopulation(), is(true));
     }
 
     @Test
     public void world_generation_with_no_seed_has_no_population_in_next_generation() {
         List<Location> seed = Collections.emptyList();
-        World world = World.generation(seed, DEFAULT_SIZE);
+        World world = World.generation(seed, WORLD_SIZE);
         assertThat(world.nextGeneration().hasPopulation(), is(false));
     }
 
     @Test
     public void world_generation_with_one_seed_has_no_population_in_next_generation() {
         List<Location> seed = Collections.singletonList(Location.of(0, 0));
-        World world = World.generation(seed, DEFAULT_SIZE);
+        World world = World.generation(seed, WORLD_SIZE);
         assertThat(world.nextGeneration().hasPopulation(), is(false));
     }
 
@@ -47,7 +47,7 @@ public class WorldTest {
                 Location.of(0, 0),
                 Location.of(1, 0)
         );
-        World world = World.generation(seed, DEFAULT_SIZE);
+        World world = World.generation(seed, WORLD_SIZE);
         assertThat(world.nextGeneration().hasPopulation(), is(false));
     }
 
@@ -58,13 +58,13 @@ public class WorldTest {
                 Location.of(1, 0),
                 Location.of(2, 0)
         );
-        World world = World.generation(seed, DEFAULT_SIZE);
+        World world = World.generation(seed, WORLD_SIZE);
         assertThat(world.nextGeneration().hasPopulation(), is(true));
     }
 
     @Test(expected = NullPointerException.class)
     public void world_generation_with_seed_being_null_throws_an_exception() {
-        World world = World.generation(null, DEFAULT_SIZE);
+        World world = World.generation(null, WORLD_SIZE);
         assertThat(world.nextGeneration().hasPopulation(), is(true));
     }
 
@@ -74,7 +74,7 @@ public class WorldTest {
                 Location.of(0, 0),
                 null
         );
-        World world = World.generation(seed, DEFAULT_SIZE);
+        World world = World.generation(seed, WORLD_SIZE);
         assertThat(world.nextGeneration().hasPopulation(), is(true));
     }
 
@@ -98,7 +98,7 @@ public class WorldTest {
                 Location.of(6, 5),
                 Location.of(6, 6)
         );
-        World world = World.generation(seed, DEFAULT_SIZE);
+        World world = World.generation(seed, WORLD_SIZE);
         assertThat(world.toString(), is(equalTo(expected)));
     }
 }
